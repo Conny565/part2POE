9794662b9103d81144db6db24119f9c7df229aec
Conny565 <st10459485@rcconnect.edu.za>
2026-05-28 16:56:01 +0200

    Add files via upload
    uploading project and file

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace part2_CyberChatBot
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
